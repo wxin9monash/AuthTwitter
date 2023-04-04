967cc8fc480321ac850dd501db97d88f88610d24
Weicheng Xing <mc.wxing@gmail.com>
2023-04-04 10:11:35 +0800

    Revert Navigation
    Revert Navigation due to pack version error

--- App.js
+++ App.js
@@ -9,10 +9,8 @@ import 'react-native-gesture-handler';
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react-native';
 import awsconfig from './src/aws-exports';
-Amplify.configure(awsconfig);
-
-// import pages
 import HomeScreen from './src/screens/Home';
+<<<<<<< HEAD
 import Tweet from './src/components/tweet';
 import SearchResultsScreen from './src/screens/SearchResults';
 import feed from './assets/data/feed';
@@ -29,6 +27,16 @@ function App() {
       <Router />
       <StatusBar style="auto" />
     </View>
+=======
+Amplify.configure(awsconfig);
+
+function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar style="auto" />
+      <HomeScreen/>
+    </SafeAreaView>
+>>>>>>> parent of 669c21a1... Tweet info display component
   );
 }
 
