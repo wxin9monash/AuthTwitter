2a2141aabdbc38c6fb9945e361b206c75580d1d0
Weicheng Xing <mc.wxing@gmail.com>
2023-04-06 00:16:07 +0800

    Add google auth

--- src/screens/LiveSearchScreen/index.js
+++ src/screens/LiveSearchScreen/index.js
@@ -1,12 +1,15 @@
 import React,{useState} from "react";
-import { View, Text, TextInput, FlatList } from "react-native";
+import { View, Text, TextInput, FlatList, Pressable } from "react-native";
 import { StyleSheet } from "react-native";
 import searchResults from '../../../assets/data/search';
+import { useNavigation } from "@react-navigation/native";
+import SearchResultsScreen from '../../screens/SearchResult';
 
 
 const LiveSearchScreen = (props) =>{
 
     const [inputText, setInputText] = useState('');
+    const navigation = useNavigation();
 
     return(
         <View style={styles.container}>
@@ -20,14 +23,29 @@ const LiveSearchScreen = (props) =>{
 
 
             {/* List of match users */}
-            <FlatList
+            {/* <FlatList
                 data={searchResults}
                 renderItem={({item})=> (
                     <View style={styles.row}>
                         <Text>{item.description}</Text>
                     </View>
                 )}
-            />
+            /> */}
+
+            {/* Button to do the search */}
+            <Pressable 
+                onPress={()=> navigation.navigate(SearchResultsScreen)}
+                style={{
+                    marginBottom: 20,
+                    backgroundColor: 'blue',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: 50,
+                    marginHorizontal: 20,
+                    borderRadius: 10,           
+                }}>
+                <Text style={{fontSize: 16, color: 'white', fontWeight: 'bold'}}>Search</Text>
+            </Pressable>
 
         </View>
     );
